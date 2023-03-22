0f5f5a2fc3a070f592edb1e681fd3b70f6c82b3b
sunmingming <sunmingming02@gyyx.cn>
2023-03-22 18:26:41 +0800

    ChatGPT for unity.

---
+++ Editor/ChatGPT/ChatGPT.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.EditorCoroutines.Editor;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ChatGPT.Editor
+{
+    public class ChatGPT
+    {
+        const string ChatgptUrl = "https://api.openai.com/v1/chat/completions";
+        const string DefaultAPIKey = "Your ChatGPT API key";
+        const string DefaultModel = "gpt-3.5-turbo";
+        const float DefaultTemperature = 0;
+        const string DefaultUserId = "user";
+        string ApiKey;
+        string UserId;
+        List<Message> messageHistory;
+        public List<Message> MessageHistory => messageHistory;
+        ChatGPTRequestData requestData;
+        UnityWebRequest webRequest;
+        public float ChatGPTRandomness { get => requestData.temperature; set { requestData.temperature = Mathf.Clamp(value, 0, 2); } }
+        public bool IsRequesting => webRequest != null && !webRequest.isDone;
+        public float RequestProgress => IsRequesting ? (webRequest.uploadProgress + webRequest.downloadProgress) / 2f : 0f;
+        public ChatGPT(string apiKey = DefaultAPIKey, string userId = DefaultUserId, string model = DefaultModel, float temperature = DefaultTemperature)
+        {
+            this.ApiKey = string.IsNullOrWhiteSpace(apiKey) ? DefaultAPIKey : apiKey;
+            this.UserId = string.IsNullOrWhiteSpace(userId) ? DefaultUserId : userId;
+            messageHistory = new List<Message>();
+            requestData = new ChatGPTRequestData(model, temperature);
+        }
+        public void SetAIPKey(string key)
+        {
+            this.ApiKey = key;
+        }
+        /// <summary>
+        /// 接着上次的话题
+        /// </summary>
+        public void RestoreChatHistory()
+        {
+            var chatHistoryJson = EditorPrefs.GetString("ChatGPT.Settings.ChatHistory", string.Empty);
+            var requestDataJson = EditorPrefs.GetString("ChatGPT.Settings.RequestData", string.Empty);
+            if (!string.IsNullOrEmpty(chatHistoryJson))
+            {
+                var jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject<ChatGPTRequestData>(requestDataJson);
+                if (jsonObj != null)
+                {
+                    requestData.messages = jsonObj.messages;
+                }
+            }
+            if (!string.IsNullOrEmpty(requestDataJson))
+            {
+                var jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Message>>(chatHistoryJson);
+                if (jsonObj != null)
+                {
+                    messageHistory = jsonObj;
+                }
+            }
+        }
+        public void SaveChatHistory()
+        {
+            var chatHistoryJson = Newtonsoft.Json.JsonConvert.SerializeObject(messageHistory);
+            var requestDataJson = Newtonsoft.Json.JsonConvert.SerializeObject(requestData);
+            EditorPrefs.SetString("ChatGPT.Settings.ChatHistory", chatHistoryJson);
+            EditorPrefs.SetString("ChatGPT.Settings.RequestData", requestDataJson);
+        }
+        public void Send(string message, Action<bool, string> onComplete = null, Action<float> onProgressUpdate = null)
+        {
+            EditorCoroutineUtility.StartCoroutine(Request(message, onComplete, onProgressUpdate), this);
+        }
+
+        public async Task<string> SendAsync(string message)
+        {
+            bool isCompleted = false;
+            string result = string.Empty;
+            Action<bool, string> onComplete = (success, str) =>
+            {
+                isCompleted = true;
+                if (success) result = str;
+            };
+
+            EditorCoroutineUtility.StartCoroutine(Request(message, onComplete, null), this);
+            while (!isCompleted)
+            {
+                await Task.Delay(10);
+            }
+            return result;
+        }
+        private IEnumerator Request(string input, Action<bool, string> onComplete, Action<float> onProgressUpdate)
+        {
+            var msg = new Message()
+            {
+                role = UserId,
+                content = input,
+            };
+            requestData.AppendChat(msg);
+            messageHistory.Add(msg);
+
+            using (webRequest = new UnityWebRequest(ChatgptUrl, "POST"))
+            {
+                var jsonDt = Newtonsoft.Json.JsonConvert.SerializeObject(requestData);
+                Debug.Log(jsonDt);
+                byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonDt);
+                webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
+                webRequest.downloadHandler = new DownloadHandlerBuffer();
+                webRequest.SetRequestHeader("Content-Type", "application/json");
+                webRequest.SetRequestHeader("Authorization", $"Bearer {this.ApiKey}");
+                webRequest.certificateHandler = new ChatGPTWebRequestCert();
+                var req = webRequest.SendWebRequest();
+                while (!webRequest.isDone)
+                {
+                    onProgressUpdate?.Invoke((webRequest.downloadProgress + webRequest.uploadProgress) / 2f);
+                    yield return null;
+                }
+
+                if (webRequest.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError($"---------ChatGPT请求失败:{webRequest.error}---------");
+                    onComplete?.Invoke(false, string.Empty);
+                }
+                else
+                {
+                    var json = webRequest.downloadHandler.text;
+                    Debug.Log(json);
+                    try
+                    {
+                        ChatCompletion result = Newtonsoft.Json.JsonConvert.DeserializeObject<ChatCompletion>(json);
+                        int lastChoiceIdx = result.choices.Count - 1;
+                        var replyMsg = result.choices[lastChoiceIdx].message;
+                        replyMsg.content = replyMsg.content.Trim();
+                        messageHistory.Add(replyMsg);
+                        onComplete?.Invoke(true, replyMsg.content);
+                    }
+                    catch (System.Exception e)
+                    {
+                        Debug.LogError($"---------ChatGPT返回数据解析失败:{e.Message}---------");
+                        onComplete?.Invoke(false, e.Message);
+                    }
+                }
+                webRequest.Dispose();
+                webRequest = null;
+            }
+        }
+        public void NewChat()
+        {
+            requestData.ClearChat();
+            messageHistory.Clear();
+        }
+        public bool IsSelfMessage(Message msg)
+        {
+            return this.UserId.CompareTo(msg.role) == 0;
+        }
+    }
+
+    class ChatGPTRequestData
+    {
+        public List<Message> messages;
+        public string model;
+        public float temperature;
+
+        public ChatGPTRequestData(string model, float temper)
+        {
+            this.model = model;
+            this.temperature = temper;
+            this.messages = new List<Message>();
+        }
+
+        /// <summary>
+        /// 同一话题追加会话内容
+        /// </summary>
+        /// <param name="chatMsg"></param>
+        /// <returns></returns>
+        public ChatGPTRequestData AppendChat(Message msg)
+        {
+            this.messages.Add(msg);
+            return this;
+        }
+        /// <summary>
+        /// 清除聊天历史(结束一个话题), 相当于新建一个聊天话题
+        /// </summary>
+        public void ClearChat()
+        {
+            this.messages.Clear();
+        }
+    }
+
+    class ChatGPTWebRequestCert : UnityEngine.Networking.CertificateHandler
+    {
+        protected override bool ValidateCertificate(byte[] certificateData)
+        {
+            //return base.ValidateCertificate(certificateData);
+            return true;
+        }
+    }
+    class Usage
+    {
+        public int prompt_tokens;
+        public int completion_tokens;
+        public int total_tokens;
+    }
+
+    public class Message
+    {
+        public string role;
+        public string content;
+    }
+
+    class Choice
+    {
+        public Message message;
+        public string finish_reason;
+        public int index;
+    }
+
+    class ChatCompletion
+    {
+        public string id;
+        public string @object;
+        public int created;
+        public string model;
+        public Usage usage;
+        public List<Choice> choices;
+    }
+}
+
---
+++ Editor/ChatGPT/ChatGPTWindow.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace ChatGPT.Editor
+{
+    public class ChatGPTWindow : EditorWindow
+    {
+        Vector2 scrollPos = Vector2.zero;
+
+        ChatGPT ai;
+        private bool settingFoldout = false;
+        string message;
+        const string aiRoleName = "AI";
+        private float chatBoxWidthRatio = 0.85f;
+        private float iconSizeRatio = 0.6f;
+        private float iconMaxSize = 100f;
+        private float chatBoxPadding = 20;
+        private float chatBoxEdgePadding = 10;
+
+        GUIStyle myChatStyle;
+        GUIStyle aiChatStyle;
+
+        GUIStyle aiIconStyle;
+        GUIStyle myIconStyle;
+        GUIStyle txtAreaStyle;
+
+        GUIContent chatContent;
+
+        bool isEditorInitialized = false;
+        private float scrollViewHeight;
+
+        string myApiKey;
+
+        [MenuItem("ChatGPT/ChatGPT Window")]
+        static void ChatGPTMenu()
+        {
+            var win = GetWindow<ChatGPTWindow>("ChatGPT");
+            win.Show();
+        }
+        private void OnEnable()
+        {
+            EditorApplication.update += OnEditorUpdate;
+            myApiKey = EditorPrefs.GetString("ChatGPT.Settings.APIKey", null);
+            ai = new ChatGPT(myApiKey);
+            ai.ChatGPTRandomness = EditorPrefs.GetFloat("ChatGPT.Settings.Temperature", 0f);
+            chatContent = new GUIContent();
+            ai.RestoreChatHistory();
+        }
+
+        private void OnEditorUpdate()
+        {
+            if (EditorApplication.isCompiling || EditorApplication.isUpdating)
+            {
+                return;
+            }
+            try
+            {
+                InitGUIStyles();
+                isEditorInitialized = true;
+                EditorApplication.update -= OnEditorUpdate;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void InitGUIStyles()
+        {
+            aiChatStyle = new GUIStyle(EditorStyles.selectionRect);
+            aiChatStyle.wordWrap = true;
+            aiChatStyle.normal.textColor = Color.white;
+            aiChatStyle.fontSize = 18;
+            aiChatStyle.alignment = TextAnchor.MiddleLeft;
+
+            myChatStyle = new GUIStyle(EditorStyles.helpBox);
+            myChatStyle.wordWrap = true;
+            myChatStyle.normal.textColor = Color.white;
+            myChatStyle.fontSize = 18;
+            myChatStyle.alignment = TextAnchor.MiddleLeft;
+
+
+            txtAreaStyle = new GUIStyle(EditorStyles.textArea);
+            txtAreaStyle.fontSize = 18;
+
+            aiIconStyle = new GUIStyle();
+            aiIconStyle.wordWrap = true;
+            aiIconStyle.alignment = TextAnchor.MiddleCenter;
+            aiIconStyle.fontSize = 18;
+            aiIconStyle.fontStyle = FontStyle.Bold;
+            aiIconStyle.normal.textColor = Color.black;
+            aiIconStyle.normal.background = EditorGUIUtility.FindTexture("sv_icon_dot5_pix16_gizmo");
+
+            myIconStyle = new GUIStyle(aiIconStyle);
+            myIconStyle.normal.background = EditorGUIUtility.FindTexture("sv_icon_dot2_pix16_gizmo");
+        }
+
+        private void OnDisable()
+        {
+            ai.SaveChatHistory();
+            SaveSettings();
+        }
+
+
+        private void OnGUI()
+        {
+            if (!isEditorInitialized) return;
+            EditorGUILayout.BeginVertical();
+            {
+                scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+                {
+                    scrollViewHeight = 0;
+                    foreach (var msg in ai.MessageHistory)
+                    {
+                        var msgRect = EditorGUILayout.BeginVertical();
+                        {
+                            EditorGUILayout.BeginHorizontal();
+                            {
+                                bool isMyMsg = ai.IsSelfMessage(msg);
+                                var labelStyle = isMyMsg ? myChatStyle : aiChatStyle;
+                                chatContent.text = msg.content;
+                                float chatBoxWidth = this.position.width * chatBoxWidthRatio;
+                                float iconSize = Mathf.Min(iconMaxSize, (this.position.width - chatBoxWidth) * iconSizeRatio);
+                                float chatBoxHeight = Mathf.Max(iconSize, chatBoxEdgePadding + labelStyle.CalcHeight(chatContent, chatBoxWidth - chatBoxEdgePadding));
+                                if (isMyMsg) { GUILayout.FlexibleSpace(); }
+                                else
+                                {
+                                    EditorGUILayout.LabelField(aiRoleName, aiIconStyle, GUILayout.Width(iconSize), GUILayout.Height(iconSize));
+                                }
+                                EditorGUILayout.SelectableLabel(msg.content, labelStyle, GUILayout.Width(chatBoxWidth), GUILayout.Height(chatBoxHeight));
+                                if (!isMyMsg) { GUILayout.FlexibleSpace(); }
+                                else
+                                {
+                                    EditorGUILayout.LabelField(msg.role, myIconStyle, GUILayout.Width(iconSize), GUILayout.Height(iconSize));
+                                }
+                                EditorGUILayout.EndHorizontal();
+                            }
+                            EditorGUILayout.EndVertical();
+                        }
+                        EditorGUILayout.Space(chatBoxPadding);
+                        scrollViewHeight += msgRect.height;
+                    }
+                    EditorGUILayout.EndScrollView();
+                }
+
+                if (ai.IsRequesting)
+                {
+                    var barWidth = position.width * 0.8f;
+                    var pBarRect = new Rect((position.width - barWidth) * 0.5f, (position.height - 30f) * 0.5f, barWidth, 30f);
+                    EditorGUI.ProgressBar(pBarRect, ai.RequestProgress, $"Progress:{ai.RequestProgress:P2}");
+                }
+                GUILayout.FlexibleSpace();
+                if (string.IsNullOrWhiteSpace(myApiKey)) EditorGUILayout.HelpBox("Please fill in the API Key in ChatGPT Settings first.", MessageType.Error);
+                if (settingFoldout = EditorGUILayout.Foldout(settingFoldout, "ChatGPT Settings:"))
+                {
+                    EditorGUILayout.BeginVertical("box");
+                    {
+                        EditorGUILayout.BeginHorizontal();
+                        {
+                            if (EditorGUILayout.LinkButton("Get API Key:", GUILayout.Width(170)))
+                            {
+                                Application.OpenURL("https://platform.openai.com/account/api-keys");
+                            }
+                            EditorGUI.BeginChangeCheck();
+                            {
+                                myApiKey = EditorGUILayout.TextField(myApiKey);
+                                if (EditorGUI.EndChangeCheck())
+                                {
+                                    ai.SetAIPKey(myApiKey);
+                                }
+                            }
+                            EditorGUILayout.EndHorizontal();
+                        }
+
+                        EditorGUILayout.BeginHorizontal();
+                        {
+                            EditorGUILayout.LabelField("Randomness", GUILayout.Width(170));
+                            ai.ChatGPTRandomness = EditorGUILayout.Slider(ai.ChatGPTRandomness, 0, 2);
+                            EditorGUILayout.EndHorizontal();
+                        }
+                        EditorGUILayout.EndVertical();
+                    }
+
+                }
+                EditorGUILayout.BeginHorizontal();
+                {
+                    message = EditorGUILayout.TextArea(message, txtAreaStyle, GUILayout.MinHeight(80));
+
+                    EditorGUI.BeginDisabledGroup(ai.IsRequesting);
+                    {
+                        if (GUILayout.Button("Send", GUILayout.MaxWidth(120), GUILayout.Height(80)))
+                        {
+                            if (!string.IsNullOrWhiteSpace(message))
+                            {
+                                ai.Send(message, OnChatGPTMessage);
+                            }
+                        }
+                        if (GUILayout.Button("New Chat", GUILayout.MaxWidth(80), GUILayout.Height(80)))
+                        {
+                            ai.NewChat();
+                        }
+                        EditorGUI.EndDisabledGroup();
+                    }
+
+                    EditorGUILayout.EndHorizontal();
+                }
+                EditorGUILayout.EndVertical();
+            }
+        }
+
+        private void OnChatGPTMessage(bool arg1, string arg2)
+        {
+            scrollPos.y = scrollViewHeight;
+            if (arg1)
+            {
+                message = string.Empty;
+            }
+            Repaint();
+        }
+
+        private void SaveSettings()
+        {
+            EditorPrefs.SetFloat("ChatGPT.Settings.Temperature", ai.ChatGPTRandomness);
+            EditorPrefs.SetString("ChatGPT.Settings.APIKey", myApiKey);
+        }
+    }
+}
+
